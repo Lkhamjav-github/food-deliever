c34a5444ed54dd9c802fc2b9797944e09da974e2
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2024-02-23 17:05:05 +0800

    footer end

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 import { Pineconewhite } from './icons/Pineconewhite'
 import { Fb } from './icons/Fb'
 import { Twit } from './icons/Twit'
-import { Ig } from './icons/ig'
+import { Ig } from './icons/Ig'
 
 import { Bgfooter } from './icons/Bgfooter'
 export const Footer = () => {
     return (
 
-        <div className='suuder flex justify-center items-center bg-[#18BA51] ' >
+        <div className='suuder flex justify-center items-center bg-[#18BA51]  py-[114px] bg-[url(/suuder.png)] ' >
             {/* <img src="./suuder.png" alt="" /> */}
-            <div className='w-[88%] flex justify-between flex-col' >
+            <div className='w-[88%] flex justify-between flex-col gap-10 px-6' >
                 <div className='flex justify-center items-center gap-2 font-bold text-white text-xl'>
                     <Pineconewhite />
                     Food Delivery
                 </div>
-                <div>
-                    <a href="">Нүүр</a>
-                    <a href="">Холбоо барих</a>
-                    <a href="">Хоолны цэс</a>
-                    <a href="">Үйлчилгээний нөхцөл</a>
-                    <a href="">Хүргэлтийн бүс</a>
-                    <a href="">Нууцлалын бодлого</a>
+                <div className='flex text-white justify-between text-base'>
+                    <a className='text-base font-semibold text-white  underline' href="">Нүүр</a>
+                    <a className='text-base font-semibold text-white  underline' href="">Холбоо барих</a>
+                    <a className='text-base font-semibold text-white  underline' href="">Хоолны цэс</a>
+                    <a className='text-base font-semibold text-white  underline' href="">Үйлчилгээний нөхцөл</a>
+                    <a className='text-base font-semibold text-white  underline' href="">Хүргэлтийн бүс</a>
+                    <a className='text-base font-semibold text-white  underline' href="">Нууцлалын бодлого</a>
                 </div>
                 <div className='flex gap-[18px] justify-center items-center'>
                     <Fb />
@@ -29,7 +29,10 @@ export const Footer = () => {
                     <Twit />
                 </div>
                 <hr />
-                <h1>© 2024 Pinecone Foods LLC <br /> Зохиогчийн эрх хуулиар хамгаалагдсан.</h1>
+                <div>
+                    <h1 className='text-white justify-center items-center flex'>© 2024 Pinecone Foods LLC</h1>
+                    <h1 className='text-white justify-center items-center flex'>Зохиогчийн эрх хуулиар хамгаалагдсан.</h1>
+                </div>
             </div >
         </div >
     )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,18 +10,18 @@ export const Header = () => {
             <div className='w-[88%] flex justify-between px-6 py-2'>
                 <div className='flex justify-center items-center gap-6'>
                     <Pinecone />
-                    <a className='text-sm font-bold' href="">Нүүр</a>
-                    <a className='text-sm font-bold' href="">Хоолны цэс</a>
-                    <a className='text-sm font-bold' href="">Хүргэлтын бүс</a>
+                    <a className='text-sm font-bold hover:text-[#18BA51]' href="">Нүүр</a>
+                    <a className='text-sm font-bold hover:text-[#18BA51]' href="">Хоолны цэс</a>
+                    <a className='text-sm font-bold hover:text-[#18BA51]' href="">Хүргэлтын бүс</a>
                 </div>
 
                 <div className='flex justify-center items-center gap-6'>
-                    <label className="flex items-center gap-2 rounded-lg border-[1px] px-4 py-2 border-[black] ">
+                    <label className="flex items-center gap-2 rounded-lg border-[1px] px-4 py-2 border-[black] hover:border-[#18BA51] ">
                         <Search />
                         <input type="text" className="grow" placeholder="Search" />
                     </label>
-                    <a href="" className='flex justify-center items-center gap-2 text-sm font-bold' ><Bag />Сагс</a>
-                    <a href="" className='flex justify-center items-center gap-2 text-sm font-bold'>< Human /> Нэвтрэх</a>
+                    <a href="" className='flex justify-center items-center gap-2 text-sm font-bold hover:text-[#18BA51]' ><Bag />Сагс</a>
+                    <a href="" className='flex justify-center items-center gap-2 text-sm font-bold hover:text-[#18BA51]'>< Human /> Нэвтрэх</a>
                 </div>
             </div>
         </div>

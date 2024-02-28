14f4c77ec605bf1e7ff6d2bb2499f3eb9472f41c
Lhamjav Sanjmyatav <lkhamjavsanjmytav@gmail.com>
2024-02-28 19:37:00 +0800

    tst

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,6 +3,7 @@ import { Fonthideeye } from './icons/Fonthideeye';
 import { Fonteye } from './icons/Fonteye';
 import { Check } from './icons/Check';
 import { Checked } from './icons/Checked';
+import axios from 'axios'
 export const Signup = () => {
     const [password, setPassword] = useState('');
     const [repassword, setRepassword] = useState('')
@@ -12,10 +13,10 @@ export const Signup = () => {
     const [address, setAddress] = useState('')
     const [retoggle, setRetoggle] = useState('hide');
     const [check, setCheck] = useState('check')
+    const BASE_URL = 'http://localhost:8080/'
     const checkBtn = () => {
         if (check === 'check') {
             setCheck('checked')
-
         }
         else {
             setCheck('check')
@@ -54,11 +55,66 @@ export const Signup = () => {
             setRetoggle('hide')
         }
     });
+    // const createUser = async (event: React.SyntheticEvent) => {
+    //         if (password === repassword) {
+    //             console.log("create user")
+    //             event.preventDefault()
+
+    //             var formData = {
+    //                 name: event.target[0].value,
+    //                 email: event.target[1].value,
+    //                 password: event.target[2].value
+    //             }
+    //             const isValid = await userSchema.isValid(formData)
+    //             // const validate = await userSchema.validate(formData)
+
+
+    //             console.log("isValid", isValid)
+
+    //             if (isValid) {
+    //                 try {
+
+    //                     const fetched = await fetch('http://localhost:8080/signup', {
+    //                         method: "POST",
+    //                         headers: {
+    //                             "Content-type": "application/json"
+    //                         },
+    //                         body: JSON.stringify({
+    //                             name,
+    //                             email,
+    //                             password
+    //                         })
+    //                     })
+
+    //                     alert("success")
+    //                 } catch (error) {
+    //                     console.error(error)
+    //                 }
+    //             }
+    //         }
+    //         else alert("password tohitohgui baina")
+    //     }
+    //     const fetchdata = async () => {
+    //         try {
+    //             const res = await fetch("http://localhost:8080/signup");
+    //             const data = await res.json()
+    //             setUsers(data)
+    //         }
+    //         catch (error) {
+    //             alert(error.message)
+    //         }
+    // }
+
+    // axios.post(BASE_URL + '/users', {
+    //     firstName: 'Finn',
+    //     lastName: 'Williams'
+    // });
+
 
     return (
         <div className='flex justify-center items-center'>
             <div className='w-[88%] max-w-[1440px] flex justify-center px-6 py-[80px] items-center'>
-                <div className='flex gap-8 justify-center items-center flex-col'>
+                <form onSubmit={createUser} className='flex gap-8 justify-center items-center flex-col'>
                     <h1 className='font-bold text-[28px]'>Бүртгүүлэх</h1>
                     <div className='py-4'>
                         <h2 className='font-normal pb-1 text-sm'>Нэр</h2>
@@ -92,14 +148,23 @@ export const Signup = () => {
                         {/* <h3 className='flex font-normal pb-1 text-sm justify-end pt-2 text-[#3F4145]'>Нууц үг сэргээх</h3> */}
                     </div>
 
+                    {/*  <form onSubmit={createUser}>
+                    <div className='flex flex-col gap-4'>
+                        <input value={name} onChange={e => setName(e.target.value)} className='border-2 bg-[#F3F4F6] rounded-lg p-4 w-[352px] border-[#D1D5DB]' type="text" placeholder='Name' />
+                        <input value={email} onChange={e => setEmail(e.target.value)} className='border-2 bg-[#F3F4F6] rounded-lg p-4 w-[352px] border-[#D1D5DB]' type="text" placeholder='Email' />
+                        <input value={password} onChange={e => setPassword(e.target.value)} className='border-2 bg-[#F3F4F6] rounded-lg p-4 w-[352px] border-[#D1D5DB]' type="password" placeholder='Password' />
+                        <input value={repassword} onChange={e => setRepassword(e.target.value)} className='border-2 bg-[#F3F4F6] rounded-lg p-4 w-[352px] border-[#D1D5DB]' type="password" placeholder='Re-password' />
+                        <button type='submit' className='bg-blue text-white rounded-[20px] h-12'>Sign up</button>
+                    </div>
+                </form> */}
                     {/* <button className='w-[384px] bg-[#EEEFF2] text-[#1C20243D] py-[14.5px] rounded'>
                         Нэвтрэх
                     </button>
                     <h3 className='flex font-normal pb-1 text-sm  pt-2 text-[#3F4145]'>Эсвэл</h3> */}
-                    <button className='w-[384px] border-[1px] border-green text-[#272727] py-[14.5px] rounded'>
+                    <button type='submit' className='w-[384px] border-[1px] border-green text-[#272727] py-[14.5px] rounded'>
                         Бүртгүүлэх
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     )
